5902e503f519b04c86953e8ecac8107d92143cc3
akashsaklani <akashsaklani2005@gmail.com>
2026-03-13 19:35:16 +0530

    Fixed insecure code to pass security scan

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-import subprocess
+def secure_function(user_input):
+    print("User input:", user_input)
 
-def insecure_function(user_input):
-    subprocess.call("echo " + user_input, shell=True)
-
-password = "123456"
-print("Hardcoded password:", password)
-
-user = input("Enter command: ")
-insecure_function(user)
+user = input("Enter text: ")
+secure_function(user)
